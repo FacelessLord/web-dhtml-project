999c8a049662ac716515bb7a522249f82fb567d7
FacelessLord <skyres21@yandex.ru>
2021-01-21 14:13:51 +0500

    Select end datetime

--- src/SearchPage.tsx
+++ src/SearchPage.tsx
@@ -6,7 +6,8 @@ import MomentUtils from "@date-io/moment";
 import moment, {Moment} from "moment";
 
 class SearchPageState {
-  datetime: Moment = moment();
+  startDatetime: Moment = moment();
+  endDatetime: Moment = moment().add(1, "hour");
   seatsCount: number = 2;
 }
 
@@ -14,6 +15,10 @@ export class SearchPage extends React.Component<{}, SearchPageState> {
   state: SearchPageState = new SearchPageState();
 
   componentDidMount() {
+  }
+
+  copyDateToDate(a: Moment, target: Moment) {
+    return target.day(a.day()).month(a.month()).year(a.year())
   }
 
   render() {
@@ -28,28 +33,48 @@ export class SearchPage extends React.Component<{}, SearchPageState> {
               id="date-picker-dialog"
               label="Дата"
               format="DD.MM.yyyy"
-              value={this.state.datetime}
+              value={this.state.startDatetime}
               onChange={(datetime: MaterialUiPickersDate) => {
-                if (datetime) this.setState({datetime})
+                if (datetime) {
+                  const newStart = this.copyDateToDate(datetime, this.state.startDatetime)
+                  const newEnd = this.copyDateToDate(datetime, this.state.endDatetime)
+
+                  this.setState({startDatetime: newStart, endDatetime: newEnd})
+                }
               }}
               KeyboardButtonProps={{
                 'aria-label': 'change date',
               }}
             />
-            <KeyboardTimePicker
-              margin="normal"
-              id="time-picker"
-              label="Время"
-              format={"HH:mm"}
-              ampm={false}
-              value={this.state.datetime}
-              onChange={(datetime: MaterialUiPickersDate) => {
-                if (datetime) this.setState({datetime})
-              }}
-              KeyboardButtonProps={{
-                'aria-label': 'change time',
-              }}
-            />
+            <Grid container justify="space-around">
+              <KeyboardTimePicker
+                margin="normal"
+                id="time-picker"
+                label="Время начала"
+                format={"HH:mm"}
+                ampm={false}
+                value={this.state.startDatetime}
+                onChange={(startDatetime: MaterialUiPickersDate) => {
+                  if (startDatetime) this.setState({startDatetime})
+                }}
+                KeyboardButtonProps={{
+                  'aria-label': 'change time',
+                }}/>
+              <KeyboardTimePicker
+                margin="normal"
+                id="time-picker"
+                label="Время окончания"
+                format={"HH:mm"}
+                ampm={false}
+                value={this.state.endDatetime}
+                onChange={(endDatetime: MaterialUiPickersDate) => {
+                  if (endDatetime && endDatetime.isAfter(this.state.startDatetime))
+                    this.setState({endDatetime})
+                }}
+                KeyboardButtonProps={{
+                  'aria-label': 'change time',
+                }}
+              /></Grid>
             <TextField
               margin="normal"
               id="seat-count"
